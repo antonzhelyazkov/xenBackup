b19eb5a2476003e25fd1d1d6d3fd4a8db277d9ed
antonzhelyazkov <antonisimo@gmail.com>
2017-08-03 09:21:19 +0300

    add cmd arguments

--- nagiosConnect.sh
+++ nagiosConnect.sh
@@ -1,47 +1,59 @@
 #!/bin/bash
 
-LOGFILE_DIR=/var/log
-NAGIOS_LOG=$LOGFILE_DIR/xenBackupNagios.inf
-LAST_RUN_FILE=$LOGFILE_DIR/xenBackup.last
-DATE_NOW=`date +%s`
-TIME_NOT_STARTED_TRESHOLD=86400
+tsNow=$(date +%s)
+timeNotStartedTreshold=20000
 
-#LOG=/root/nagiosConnect.log
-RUNNING=0
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
 
+zimbraScriptName="zimbraSnapshot.sh"
 ########################################
 
+USAGE="Usage: $(basename $0) [-h] [-n nagios log] [-l lar run]\n
+        -n [FILE] Path to nagios log - zimbraSnapshot.log.\n
+        -l [FILE] Lastrun file.\n
+        Please use full paths!
+"
 
-if [ -f $NAGIOS_LOG ] && [ -f $LAST_RUN ]; then
-        PID=$(sed '1q;d' $NAGIOS_LOG)
-        if [ -f /proc/$PID/status ]; then
-                RUNNING=1
+if [ "$1" == "-h" ] || [ "$1" == "" ] || [ "$#" != 4 ]; then
+        echo -e $USAGE
+        exit $STATE_UNKNOWN
+fi
+
+while getopts n:l: option
+do
+        case "${option}"
+                in
+                n) nagiosLog=${OPTARG};;
+                l) lastRun=${OPTARG};;
+        esac
+done
+
+if [ ! -f $lastRun ] && [ ! -f $nagiosLog ]; then
+        echo "Last run file $lastRun and Nagios inf $nagiosLog not found! This may be first run of $zimbraScriptName or sometning is wrong"
+        exit $STATE_UNKNOWN
+fi
+
+if [ -f $nagiosLog ]; then
+        pidNumber=$(sed '1q;d' $nagiosLog)
+        if [ -f /proc/$pidNumber/status ]; then
+                echo "OK $zimbraScriptName is running | running=1"
+                exit $STATE_OK
         else
-                RUNNING=0
-        fi
-elif [ ! -f $NAGIOS_LOG ] && [ -f $LAST_RUN ]; then
-        echo "OK backup script finished without errors | running=0"
-        exit 0
-fi
-
-if [ ! -f $LAST_RUN ]; then
-        echo "CRITICAL last run file $LAST_RUN not found" >> $LOG
-        exit 2
-else
-        LAST_RUN_TIME=$(sed '1q;d' $LAST_RUN_FILE)
-        TIME_NOT_STARTED=$(( $DATE_NOW - $LAST_RUN_TIME ))
-        if [ $TIME_NOT_STARTED -gt $TIME_NOT_STARTED_TRESHOLD ] && [ $RUNNING -eq 0 ]; then
-                echo "CRITICAL script has not started more than $TIME_NOT_STARTED"
-                exit 2
+                echo "WARNING $zimbraScriptName is NOT running and INF $nagiosLog exists | running=0"
+                exit $STATE_WARNING
         fi
 fi
 
-if [ -f $NAGIOS_LOG ] && [ $RUNNING -eq 0 ]; then
-        echo "WARNING backup script finished with errors | running=0"
-        exit 1
-fi
-
-if [ -f $NAGIOS_LOG ] && [ $RUNNING -eq 1 ]; then
-        echo "OK backup script is running | running=1"
-        exit 0
+if [ -f $lastRun ] && [ ! -f $nagiosLog ]; then
+        lastRunTS=$(sed '1q;d' $lastRun)
+        if [ $(($lastRunTS + $timeNotStartedTreshold)) -lt $tsNow ]; then
+                echo "WARNING backup script not started more than $(($tsNow - $lastRunTS)) | running=0"
+                exit $STATE_WARNING
+        else
+                echo "OK backup finished without errors $(($tsNow - $lastRunTS)) seconds ago | running=0"
+                exit $STATE_OK
+        fi
 fi
